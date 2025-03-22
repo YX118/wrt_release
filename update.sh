2b8b40969bbfc28e80ca2e0fe1fd0efb52ebef94
YX118 <1234125@qq.com>
2025-03-22 13:03:04 +0800

    Update update.sh
    Signed-off-by: YX118 <1234125@qq.com>

--- update.sh
+++ update.sh
@@ -90,6 +90,7 @@ remove_unwanted_packages() {
         "luci-app-ssr-plus" "luci-app-vssr" "luci-theme-argon" "luci-app-daed" "luci-app-dae"
         "luci-app-alist" "luci-app-argon-config" "luci-app-homeproxy" "luci-app-haproxy-tcp"
         "luci-app-openclash" "luci-app-mihomo" "luci-app-appfilter" "luci-app-gecoosac" "luci-app-pushbot"
+        "luci-app-poweroffdevice"
     )
     local packages_net=(
         "haproxy" "xray-core" "xray-plugin" "dns2socks" "alist" "hysteria"
@@ -141,7 +142,8 @@ install_small8() {
         luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
         luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
         luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf \
-        easytier luci-app-easytier luci-app-gecoosac luci-app-pushbot luci-app-fileassistant luci-app-usb-printer
+        easytier luci-app-easytier luci-app-gecoosac luci-app-pushbot luci-app-fileassistant luci-app-usb-printer \
+        luci-app-poweroffdevice
 }
 
 install_feeds() {
