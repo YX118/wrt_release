0733eb047e9c50226ad9b98cb1b5e07c29b0f5dc
YX118 <yx@yxoyx.xyz>
2025-06-07 11:50:33 +0800

    Update update.sh
    Signed-off-by: YX118 <yx@yxoyx.xyz>

--- update.sh
+++ update.sh
@@ -156,7 +156,7 @@ install_small8() {
         luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
         luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
         luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf \
-        easytier luci-app-easytier msd_lite luci-app-msd_lite
+        easytier luci-app-easytier msd_lite luci-app-msd_lite luci-app-pushbot luci-app-fileassistant
 }
 
 install_feeds() {
@@ -447,7 +447,7 @@ update_nss_pbuf_performance() {
 set_build_signature() {
     local file="$BUILD_DIR/feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js"
     if [ -d "$(dirname "$file")" ] && [ -f $file ]; then
-        sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ build by ZqinKing')/g" "$file"
+        sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ build by YX')/g" "$file"
     fi
 }
 
